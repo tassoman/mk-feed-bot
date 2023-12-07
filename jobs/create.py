7eec45dd8b0d75ded7c9a6964d99129dad624a70
tassoman <tassoman@gmail.com>
2023-12-07 07:29:08 +0100

    better API exceptions handling

--- jobs/create.py
+++ jobs/create.py
@@ -4,6 +4,7 @@ import sqlite3
 import time
 import os
 from misskey import Misskey
+from misskey.exceptions import MisskeyAPIException
 from dotenv import load_dotenv
 from jobs.sentiment import get_sentiment
 
@@ -35,21 +36,24 @@ def publish_note():
         for d in data:
             sentiment = get_sentiment(d[4] + d[5])
             text = "\n<b>" + d[4] + "</b>\n" + d[5] + " <i>(" +d[1] + ")</i>\n\n" + d[3]
-            cw = None if sentiment >= 0 else ":nsfw: News article"
+            cw = None if sentiment >= 0 else ":nsfw: News article flagged CW"
             time.sleep(2)
-            api = mk.notes_create(
-                text=text,
-                visibility=visibility,
-                local_only=local_only,
-                cw=cw
-            )
-            n_id = api['createdNote']['id']
-            n_at = int(datetime.strptime(
-                api['createdNote']['createdAt'], '%Y-%m-%dT%H:%M:%S.%fZ'
-            ).timestamp())
+            try:
+                api = mk.notes_create(
+                    text=text,
+                    visibility=visibility,
+                    local_only=local_only,
+                    cw=cw
+                )
+                n_id = api['createdNote']['id']
+                n_at = int(datetime.strptime(
+                    api['createdNote']['createdAt'], '%Y-%m-%dT%H:%M:%S.%fZ'
+                ).timestamp())
 
-            c.execute('''
-                UPDATE news SET sentiment = ?, noteId = ?, notedAt = ? WHERE id = ?
-            ''', (sentiment, n_id, n_at, d[0]))
-            db.commit()
+                c.execute('''
+                    UPDATE news SET sentiment = ?, noteId = ?, notedAt = ? WHERE id = ?
+                ''', (sentiment, n_id, n_at, d[0]))
+                db.commit()
+            except MisskeyAPIException as e:
+                print(f"MK API error: {e}")
         db.close()
